466298f579ffdde4a6f16cd5a8429e26025d2414
Carl Simonson <simonsonc@gmail.com>
2014-03-12 20:08:10 -0500

    Set up skeleton for KbdBacklight class

---
+++ src/xfpm-kbd-backlight.c
@@ -0,0 +1,65 @@
+/*
+ * * Copyright (C) 2014 Carl Simonson <simonsonc@gmail.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "xfpm-kbd-backlight.h"
+
+static void xfpm_kbd_backlight_finalize (GObject *object);
+
+#define XFPM_KBD_BACKLIGHT_GET_PRIVATE(o) \
+(G_TYPE_INSTANCE_GET_PRIVATE ((o), XFPM_TYPE_KBD_BACKLIGHT, XfpmKbdBacklightPrivate))
+
+struct XfpmKbdBacklightPrivate
+{
+};
+
+G_DEFINE_TYPE (XfpmKbdBacklight, xfpm_kbd_backlight, G_TYPE_OBJECT)
+
+static void
+xfpm_kbd_backlight_class_init (XfpmKbdBacklightClass *klass)
+{
+    GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+    object_class->finalize = xfpm_kbd_backlight_finalize;
+
+    g_type_class_add_private (klass, sizeof (XfpmKbdBacklightPrivate));
+}
+
+static void
+xfpm_kbd_backlight_init (XfpmKbdBacklight *backlight)
+{
+}
+
+static void
+xfpm_kbd_backlight_finalize (GObject *object)
+{
+    G_OBJECT_CLASS (xfpm_kbd_backlight_parent_class)->finalize (object);
+}
+
+XfpmKbdBacklight *
+xfpm_kbd_backlight_new (void)
+{
+    XfpmKbdBacklight *self = NULL;
+    self = g_object_new (XFPM_TYPE_KBD_BACKLIGHT, NULL);
+    return self;
+}
